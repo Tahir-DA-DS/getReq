5d9638d10a71b30db333b14cd3c74bcdaf1c246f
Tahir-DA-DS <tahir.adeleye@gmail.com>
2022-10-29 18:17:02 +0100

    changes made

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 const express = require("express")
 const cors = require ("cors")
-const http = require("http")
+
 
 const app = express()
-
-http.get('http://localhost:8080/', (res) => {
-
-    let myData=''
-    res.on('data', (chunk)=>{myData+=chunk})
-    res.on('end', ()=>{
-      try {const parsedData = JSON.parse(myData)
-        console.log(parsedData)
-      }catch(error){
-        console.error(error)
-      }
-    })
-})
-
 app.use(cors())
 
-const server= http.createServer((req, res)=>{
-    
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    const data ={slakUsername:'Tahir. A',
-    Backend:true,
-    Age:26,
-    Bio:'I am Tahir popularly refered to as Ola, I am on the Backend Track in HNGi9 participating from a Abuja and I am also a graduate of Statistics'
-  }
-    res.end(JSON.stringify(data));
-    
-
+app.get("/", cors(), (req, res) => {
+  res.json({
+    slackUsername: "Tahir. A",
+    backend: true,
+    age: 26,
+    bio: "I am Tahir Adeleye a backend Intern with HNGi9 and a Mathematics Graduate .",
+  })
 })
 
-server.listen(process.env.PORT || 8080)
+
+
+    
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is Up and Running")
+})
